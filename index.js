a731dfa9e41530f7717163c5f84e709fc8aee344
cd <coraldarling@outlook.com>
2018-07-16 22:52:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var recipes = {};
 
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, {[key]: value})
+};
 
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
