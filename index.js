3c4a4dbdc7a57e2ee63d4453840480d2c0cabf46
cd <coraldarling@outlook.com>
2018-07-16 22:26:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var recipes = {};
 
-updateObjectWithKeyAndValue(obj, key, value) {
-  return Object.assign({}, obj, {[key]: value})
-};
-
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, key)
   delete newObj.key
